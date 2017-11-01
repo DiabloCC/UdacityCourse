be417b10c2caf9bdcf1761f134b03f78d601fef3
diablocc <chh986212@126.com>
2017-11-01 23:00:40 +0800

    parallel & orthogonal

--- p3/excercises.py
+++ p3/excercises.py
@@ -17,10 +17,36 @@ def vector_ex():
   # print Vector([1.996,3.108,-4.554]).normalize()
   
   # 8
-  print Vector([7.887,4.138]).dot_product(Vector([-8.802,6.776]))
-  print Vector([-5.955,-4.904,-1.874]).dot_product(Vector([-4.496,-8.755,7.103]))
-  print Vector([3.183,-7.627]).inner_angle(Vector([-2.668,5.319]))
-  print Vector([7.350,0.221,5.188]).inner_angle(Vector([2.751,8.259,3.985]), True)
+  # print Vector([7.887,4.138]).dot_product(Vector([-8.802,6.776]))
+  # print Vector([-5.955,-4.904,-1.874]).dot_product(Vector([-4.496,-8.755,7.103]))
+  # print Vector([3.183,-7.627]).inner_angle(Vector([-2.668,5.319]))
+  # print Vector([7.350,0.221,5.188]).inner_angle(Vector([2.751,8.259,3.985]), True)
 
+  # 10
+  print "*"*10
+  print Vector([-7.579,-7.880]).parallel(Vector([22.737,23.640]))
+  print Vector([-7.579,-7.880]).parallel2(Vector([22.737,23.640]))
+  print Vector([-7.579,-7.880]).parallel3(Vector([22.737,23.640]))
+  print "-"*10
+  print Vector([-7.579,-7.880]).orthogonal(Vector([22.737,23.640]))
+  print "*"*10+"\n"
+  print Vector([-2.029,9.970,4.172]).parallel(Vector([-9.231,-6.639,-7.245]))
+  print Vector([-2.029,9.970,4.172]).parallel2(Vector([-9.231,-6.639,-7.245]))
+  print Vector([-2.029,9.970,4.172]).parallel3(Vector([-9.231,-6.639,-7.245]))
+  print "-"*10
+  print Vector([-2.029,9.970,4.172]).orthogonal(Vector([-9.231,-6.639,-7.245]))
+  print "*"*10+"\n"
+  print Vector([-2.328,-7.284,-1.214]).parallel(Vector([-1.821,1.072,-2.940]))
+  print Vector([-2.328,-7.284,-1.214]).parallel2(Vector([-1.821,1.072,-2.940]))
+  print Vector([-2.328,-7.284,-1.214]).parallel3(Vector([-1.821,1.072,-2.940]))
+  print "-"*10
+  print Vector([-2.328,-7.284,-1.214]).orthogonal(Vector([-1.821,1.072,-2.940]))
+  print "*"*10+"\n"
+  print Vector([2.118,4.827]).parallel(Vector([0,0]))
+  print Vector([2.118,4.827]).orthogonal(Vector([0,0]))
+  print "-------------------"
+  print Vector([3,4]).parallel(Vector([4.5,6.0]))
+  print Vector([3,4]).parallel3(Vector([4.5,6.0]))
+  
 if __name__ == '__main__':
   vector_ex()
--- p3/vector.py
+++ p3/vector.py
@@ -1,82 +1,112 @@
 from math import acos, pi, sqrt
 from decimal import Decimal, getcontext
+
 class Vector(object):
-    def __init__(self, coordinates):
-      try:
-        if not coordinates:
-            raise ValueError
-        self.coordinates = tuple([Decimal(x) for x in coordinates])
-        self.dimension = len(coordinates)
+  def __init__(self, coordinates):
+    try:
+      if not coordinates:
+          raise ValueError
+      self.coordinates = tuple([Decimal(x) for x in coordinates])
+      self.dimension = len(coordinates)
 
-      except ValueError:
-        raise ValueError('The coordinates must be nonempty')
+    except ValueError:
+      raise ValueError('The coordinates must be nonempty')
 
-      except TypeError:
-        raise TypeError('The coordinates must be an iterable')
+    except TypeError:
+      raise TypeError('The coordinates must be an iterable')
 
+  def __str__(self):
+    return 'Vector: {}'.format(self.coordinates)
 
-    def __str__(self):
-      return 'Vector: {}'.format(self.coordinates)
-
-
-    def __eq__(self, v):
-      return self.coordinates == v.coordinates
-    
-    def iszero(self):
-      return self.coordinates == tuple([Decimal(0.0)] * self.dimension)
-        
-    def plus(self, v):
-      if isinstance(v, Vector):
-        if self.dimension == v.dimension :
-          return Vector([x+y for x, y in zip(self.coordinates, v.coordinates)])
-        else:
-          raise ValueError('dimension not match.')
-      else:
-        raise TypeError('not a Vector.')
+  def __eq__(self, v):
+    return self.coordinates == v.coordinates
   
-    def minus(self, v):
-      if isinstance(v, Vector):
-        if self.dimension == v.dimension :
-          return Vector([x-y for x, y in zip(self.coordinates, v.coordinates)])
-        else:
-          raise ValueError('dimension not match.')
+  def iszero(self, tolerance=1e-10):
+    return self.magnitude()<tolerance
+      
+  def plus(self, v):
+    if isinstance(v, Vector):
+      if self.dimension == v.dimension :
+        return Vector([x+y for x, y in zip(self.coordinates, v.coordinates)])
       else:
-        raise TypeError('not a Vector.')
-  
-    def scalar_multiply(self, scalar):
-      try:
-        return Vector([Decimal(scalar) * x for x in self.coordinates])
-      except Exception:
-        raise TypeError('{0} is not a number'.format(scalar))
-        
-    def magnitude(self):
-      if self.iszero():
-        return 0
+        raise ValueError('dimension not match.')
+    else:
+      raise TypeError('not a Vector.')
+
+  def minus(self, v):
+    if isinstance(v, Vector):
+      if self.dimension == v.dimension :
+        return Vector([x-y for x, y in zip(self.coordinates, v.coordinates)])
       else:
-        return Decimal(sqrt(sum([x**2 for x in self.coordinates])))
-        
-    def normalize(self):
-      if self.iszero():
-        raise ValueError("Can't normalize a zero vector.")
-      else:
-        return self.scalar_multiply(Decimal(1.0)/self.magnitude())
-        
-    def dot_product(self, v):
-      if not isinstance(v, Vector):
-        raise TypeError('not a Vector')
-      else:
-        if self.dimension != v.dimension:
-          raise ValueError('dimension not match.')
-        else:
-          return sum([x*y for x,y in zip(self.coordinates,v.coordinates)])            
-    
-    def inner_angle(self, v, in_degree=False):
-      if not isinstance(v, Vector):
-        raise TypeError('not a Vector')
+        raise ValueError('dimension not match.')
+    else:
+      raise TypeError('not a Vector.')
+
+  def scalar_multiply(self, scalar):
+    try:
+      return Vector([Decimal(scalar) * x for x in self.coordinates])
+    except Exception:
+      raise TypeError('{0} is not a number'.format(scalar))
+      
+  def magnitude(self):
+      return Decimal(sqrt(sum([x**2 for x in self.coordinates])))
+      
+  def normalize(self):
+    if self.iszero():
+      raise ValueError("Can't normalize a zero vector.")
+    else:
+      return self.scalar_multiply(Decimal(1.0)/self.magnitude())
+      
+  def dot_product(self, v):
+    if not isinstance(v, Vector):
+      raise TypeError('not a Vector')
+    else:
       if self.dimension != v.dimension:
         raise ValueError('dimension not match.')
-      ret = acos(self.normalize().dot_product(v.normalize()))
-      if in_degree:
-        return ret/pi*180
       else:
-        return ret
+        return sum([x*y for x,y in zip(self.coordinates,v.coordinates)])            
+  
+  def inner_angle(self, v, in_degree=False, tolerance=1e-10):
+    if not isinstance(v, Vector):
+      raise TypeError('not a Vector')
+    if self.dimension != v.dimension:
+      raise ValueError('dimension not match.')
+    d = self.normalize().dot_product(v.normalize())
+    if abs(abs(d)-1) < tolerance:
+      d = 1 if d>0 else -1
+    if abs(d)<tolerance:
+      d = 0
+    if in_degree:
+      return acos(d)/pi*180
+    else:
+      return acos(d)
+      
+  def parallel(self, v):
+    if not isinstance(v, Vector):
+      raise TypeError('not a Vector')
+    if self.iszero() or v.iszero():
+      return True
+    v1 = self.normalize()
+    v2 = v.normalize()
+    return (v1.minus(v2).iszero() or 
+            v1.plus(v2).iszero())
+    
+  def parallel2(self, v):
+    if not isinstance(v, Vector):
+      raise TypeError('not a Vector')
+    if self.iszero() or v.iszero():
+      return True
+    return self.scalar_multiply(v.coordinates[0] / self.coordinates[0]).minus(v).iszero()
+    
+  def parallel3(self, v):
+    if not isinstance(v, Vector):
+      raise TypeError('not a Vector')
+    return (self.iszero() or 
+            v.iszero() or
+            self.inner_angle(v) == 0 or
+            self.inner_angle(v) == pi)
+    
+  def orthogonal(self, v, tolerance=1e-10):
+    if not isinstance(v, Vector):
+      raise TypeError('not a Vector')
+    return abs(self.dot_product(v)) < tolerance
